377409973dd07c68c95d6ff49cd88c7d75bf02db
Penguin8885 <penguin.8885@gmail.com>
2018-02-09 21:24:54 +0900

    add sample for getting timeline limit

---
+++ sample_get_timeline_limit.py
@@ -0,0 +1,26 @@
+from requests_oauthlib import OAuth1Session
+
+if __name__ == '__main__':
+    CK = 'XXXXXXXXXXXXXXXXXXXXXX'                             # Consumer Key
+    CS = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'         # Consumer Secret
+    AT = 'XXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' # Access Token
+    AS = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'         # Accesss Token Secert
+
+    # タイムライン取得用のURL
+    url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+
+    # とくにパラメータは無い
+    params = {}
+
+    # OAuth で GET method
+    twitter = OAuth1Session(CK, CS, AT, AS)
+    req = twitter.get(url, params = params)
+
+    # レスポンスを確認
+    if req.status_code == 200:
+        limit = req.headers['x-rate-limit-remaining']   # API残り
+        reset = req.headers['x-rate-limit-reset']       # API制限の更新時刻 (UNIX time)
+        print ("API remain: " + limit)
+        print ("API reset: " + reset)
+    else:
+        print ("Error: %d" % req.status_code)
